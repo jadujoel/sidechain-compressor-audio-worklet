ab84aba615ffd816d3360a1bc6b49cbced6d5915
Joel Lof <jlof@evolution.com>
2022-10-03 00:08:52 +0200

    revert: go back to the old audio-context starter

--- packages/sidechain-compressor-app/src/utils/audio-context.ts
+++ packages/sidechain-compressor-app/src/utils/audio-context.ts
@@ -3,28 +3,24 @@ import { trySync } from "./tryit"
 export async function createAudioContext(contextOptions?: AudioContextOptions) {
     // check if we need to install polyfill and
     // Make sure audioContext has been allowed to start from user interaction
-    let [, audioContext] = trySync(() => new AudioContext(contextOptions))()
+    const [, audioContext] = trySync(() => new AudioContext(contextOptions))()
 
     return new Promise<AudioContext>((resolve) => {
         if (audioContext?.state === "running") {
             resolve(audioContext)
         }
         // if audio context was not allowed to be started we need to wait for an user interaction before we can initialize
-        function tryToStart(){
-            if (audioContext) {
-                if (audioContext?.state === "running") {
-                    resolve(audioContext)
-                }
-                else {
-                    audioContext.resume()
-                }
-            }
-            else {
-                audioContext = new AudioContext(contextOptions)
-                tryToStart()
-            }
-            setTimeout(tryToStart, 200)
+        const oninteraction = () => {
+            const audioContext = new AudioContext(contextOptions)
+            window.removeEventListener("click", oninteraction)
+            window.removeEventListener("touchstart", oninteraction)
+            window.removeEventListener("keypress", oninteraction)
+            audioContext.resume()
+            resolve(audioContext)
         }
-        tryToStart()
+
+        window.addEventListener("click", oninteraction)
+        window.addEventListener("keypress", oninteraction)
+        window.addEventListener("touchstart", oninteraction)
     })
 }
