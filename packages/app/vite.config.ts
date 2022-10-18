7f0708468640b0ba6d1154d574fa868c57bdc533
Joel Lof <jlof@evolution.com>
2022-10-18 12:24:32 +0200

    fix: update vite config

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/sidechain-compressor-audio-worklet/',
     plugins: [react()],
     optimizeDeps: {
         include: ['sidechain-compressor', 'audio-worklet-helpers'],
